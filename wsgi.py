e31bfe255037ca899acbba8b3f3bf6b875e3e4a9
kamalynx <kamalynxpro@gmail.com>
2024-07-16 00:45:15 +0500

    new wsgi/dev modules

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    serve(app, unix_socket='/tmp/waitress.sock', unix_socket_perms='666')
